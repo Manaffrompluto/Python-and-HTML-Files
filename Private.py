46b492a7f2a273f905a476e4691c47c534f10ba5
Manaffrompluto <manafabdullahramin@gmail.com>
2026-08-29 21:49:37 +0600

    private
    da terminal shows an attribute error which shows what __urvariablename which makes ur variable private does

---
+++ Private.py
@@ -0,0 +1,13 @@
+class age:
+    __hmm = 27;
+
+    def __math(self):
+        print(f"Im {age}.")
+
+    def hello(self):
+        print(f"Da private variable value iz {age.__hmm}.")
+
+stoopid = age()
+stoopid.hello()
+stoopid.__math
+    
