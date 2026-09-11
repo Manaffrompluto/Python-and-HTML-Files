b598d62b091d82e068a10879cf5a758053f8adb6
Manaffrompluto <manafabdullahramin@gmail.com>
2026-09-11 21:58:00 +0600

    black screen
    just starting out with pygame

---
+++ game.py
@@ -0,0 +1,10 @@
+import pygame
+pygame.init()
+scr = pygame.display.set_mode((400, 500))
+done = False
+
+while not done:
+    for event in pygame.event.get():
+        if type == pygame.QUIT:
+            pygame.quit()
+pygame.display.flip()
